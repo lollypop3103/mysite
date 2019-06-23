72b537c9a9cee9ac5033bd4bcaeede75a58d73b6
lollypop3103 <lollypop3103@gmail.com>
2019-06-23 16:24:46 +0900

    fix

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import BlogTemp from "@/components/BlogTemp.vue";
-// import BlogPost from "@/components/BlogPost.vue";
 
 Vue.use(Router);
 
