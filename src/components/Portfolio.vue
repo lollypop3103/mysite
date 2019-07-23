eb3ed4e1c545750a144843e0155bbc962bfc645a
lollypop3103 <lollypop3103@gmail.com>
2019-07-24 00:12:13 +0900

    update portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,8 +2,27 @@
   <section class="section">
     <div class="container">
       <div class="content is-medium">
+        <!--
         <div id="portfolio">
-          <h1>TBD</h1>
+          <section class="section">
+            <div class="container">
+              <div class="content"></div>
+            </div>
+          </section>
+        </div>
+        -->
+        <div id="career">
+          <div class="content">
+            <h1 class="title">Career</h1>
+            <ul>
+              <li>2016/03 大学院卒(工学)</li>
+              <li>2016/04 WEB系企業へ入社</li>
+              <li>
+                2018/03-2018/09 副業にて都内人工知能関連企業の手伝いを行う
+              </li>
+              <li>2019/06- 副業にて都内音楽系企業の手伝いを行う</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
