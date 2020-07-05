c403a16170756199fb783bbdafdc6368744b1fa5
lollypop3103 <lollypop3103@gmail.com>
2020-07-05 23:31:50 +0900

    feat: create Contact and Works page

---
+++ src/pages/Works.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="contact">
+    <portfolio />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Portfolio from '@/components/Portfolio.vue';
+
+export default Vue.extend({
+  components: {
+    Portfolio,
+  },
+  metaInfo() {
+    return {
+      title: 'Portfolio',
+      meta: [{ name: 'author', content: 'Cheers Beer' }],
+    };
+  },
+});
+</script>
+<style lang="stylus" scoped>
+@import '../../node_modules/bulma.styl/bulma.styl';
+
+.font-awesome-icon {
+  margin-right: 8px;
+}
+
+a {
+  color: #4a4a4a;
+}
+</style>
