9102cd0efbbf5318a7e562555ff1348cb0f09b59
lollypop3103 <lollypop3103@gmail.com>
2022-07-01 11:47:05 +0900

    feat: revival

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aec30a0d8432421be55099ae954c16ea'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '33b2fd8b-1349-444d-8452-b90cc8cc7e89'
+};
